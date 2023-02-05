1c8925c2124c2037770f1f1276a490253a35febe
楊遠徵 <codes@189.cn>
2023-02-05 18:12:44 +0800

    SN

--- devices/ipq807x_ipq60xx/diy.sh
+++ devices/ipq807x_ipq60xx/diy.sh
@@ -6,21 +6,23 @@ SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
 rm -rf target/linux/ipq807x
 mv -f ../feeds/ipq807x/ipq807x target/linux/
+rm -rf package/feeds
+./scripts/feeds install -a -p gl_feeds_common -f
+./scripts/feeds install -a -p ipq807x -f
 ./scripts/feeds install -a -p wifi_ax -f
+./scripts/feeds install -a -p womade -f
 ./scripts/feeds install -a
-
-rm -rf package/feeds/wifi_ax/hostapd
 
 sed -i "/gl_feeds_common/d" feeds.conf
 sed -i "/ipq807x/d" feeds.conf
 sed -i "/wifi_ax/d" feeds.conf
 
-rm -rf package/feeds/womade/{firewall,rtl*,base-files,netifd,nft-fullcone,mbedtls,oaf,shortcut-fe,simulated-driver,fast-classifier,fullconenat}
+rm -rf package/feeds/womade/{firewall,rtl*,base-files,netifd,fullconenat-nft,fullconenat-nft,mbedtls,oaf,shortcut-fe,simulated-driver,fast-classifier,fullconenat}
 
 rm -rf package/kernel/{ath10k-ct,mt76,rtl8812au-ct}
-rm -rf feeds/packages/net/xtables-addons package/feeds/packages/openvswitch package/feeds/routing/batman-adv
+rm -rf feeds/packages/net/xtables-addons package/feeds/packages/{openvswitch,ksmbd} package/feeds/routing/batman-adv
 
-rm -rf devices/common/patches/{glinet,imagebuilder.patch,iptables.patch,targets.patch,kernel-defaults.patch,disable_flock.patch}
+rm -rf devices/common/patches/{glinet,imagebuilder.patch,fix.patch,iptables.patch,targets.patch,kernel-defaults.patch,disable_flock.patch}
 
 rm -rf toolchain/musl
 
